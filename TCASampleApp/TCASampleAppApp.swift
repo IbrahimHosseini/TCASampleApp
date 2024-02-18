bc6ccceb622de802e5a279c698283acba9737c81
Ibrahim <hosseini@techranco.com>
2024-02-18 17:40:10 +0330

    Initial Commit

---
+++ TCASampleApp/TCASampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleAppApp.swift
+//  TCASampleApp
+//
+//  Created by Ibrahim on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
